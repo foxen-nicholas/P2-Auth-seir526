cc97615aef74772e10cd1cd2d55d48eb5d0ffa7b
Nick Foxen <foxen.nicholas@gmail.com>
2020-06-30 14:46:12 -0700

    session middleware added

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const ejsLayouts = require('express-ejs-layouts')
 const helmet = require('helmet');
 const session = require('express-session');
 const flash = require("flash");
+const passport = require('./config/ppConfig');
+const db = require('./models');
+// want to add alink to our customer middleware for isLoggedIn
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 
 // App setup
@@ -19,6 +23,33 @@ app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 app.use(helmet());
+
+// create new instance of class Sequelize Store
+const sessionStore = new SequelizeStorre({
+  db: db.sequelize,
+  expiration: 1000 * 60 * 30
+})
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: true
+}))
+// Need next bit to run the previous bit
+sessionStore.sync();
+// Initialize flash message, passport and session info
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(function(req,res, next) {
+  res.locals.alert = req.flash();
+  res.locals.currentUser = req.user;
+
+  next();
+})
+
 // ROUTES
 app.get('/', function(req, res) {
   // check to see if the user logged in
